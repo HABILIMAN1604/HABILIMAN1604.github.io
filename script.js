2a78132a4249111420fa1be7e68b8c5570c531db
HABILIMAN1604 <152095546+HABILIMAN1604@users.noreply.github.com>
2023-11-29 06:24:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
+/*validate form fields before submission*/
 document.addEventListener('DOMContentLoaded', function () {
     var form = document.getElementById('contact-form');
 
     form.addEventListener('submit', function (event) {
-    // Validate form fields
         var name = form.querySelector('#name').value.trim();
         var email = form.querySelector('#email').value.trim();
         var message = form.querySelector('#message').value.trim();
@@ -17,6 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
+/*Animations on hover for project cards.*/
 document.addEventListener('DOMContentLoaded', function () {
     var projectCards = document.querySelectorAll('.project');
 
@@ -31,6 +32,10 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+
+
+
+/*colour change for navigation bar*/
 document.addEventListener("DOMContentLoaded", function () {
     const navLinks = document.querySelectorAll("nav a");
 
@@ -46,11 +51,55 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+
+
+/*hamburger menun button for better mobiled view*/
 document.addEventListener('DOMContentLoaded', function () {
-    // Add an event listener to the hamburger menu button
     document.getElementById('mobile-menu').addEventListener('click', function () {
-        // Toggle the 'active' class on the navigation menu
         document.querySelector('nav ul').classList.toggle('active');
+    });
+});
+
+
+
+
+/*added animations for project cards*/
+document.addEventListener("DOMContentLoaded", function () {
+    var projects = document.querySelectorAll(".project");
+
+    projects.forEach(function (project) {
+        project.addEventListener("click", function () {
+            var projectInfo = this.querySelector(".project-info");
+            if (projectInfo) {
+                projectInfo.classList.toggle("visible");
+            }
+        });
+
+        project.addEventListener("mouseover", function () {
+            var projectImage = this.querySelector("img");
+            var projectInfo = this.querySelector(".project-info");
+
+            if (projectImage) {
+                projectImage.style.transform = "scale(1.1)";
+            }
+
+            if (projectInfo) {
+                projectInfo.style.opacity = 1;
+            }
+        });
+
+        project.addEventListener("mouseout", function () {
+            var projectImage = this.querySelector("img");
+            var projectInfo = this.querySelector(".project-info");
+
+            if (projectImage) {
+                projectImage.style.transform = "scale(1)";
+            }
+
+            if (projectInfo) {
+                projectInfo.style.opacity = 0;
+            }
+        });
     });
 });
 
